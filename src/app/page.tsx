a1493fea953f4f83184713b662b01729d9e7915b
Jonathan Bangert <jonathan@bangert.dk>
2024-11-24 16:07:53 -0700

    Fix types

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { useState } from 'react'
 import Link from 'next/link'
 import { motion } from 'framer-motion'
 import { Route } from 'lucide-react'
-import { Button } from "@/components/ui/button"
+import { Button } from '../components/ui/button'
 
 export default function StartPage() {
   const [isHovered, setIsHovered] = useState(false)
