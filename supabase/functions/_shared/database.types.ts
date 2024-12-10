86251ce62ce7b9fca63badaee823694dc1efb8c5
Ell1ott <85990359+Ell1ott@users.noreply.github.com>
2024-12-10 14:15:35 +1300

    Refactor: database.types.ts formatting

--- supabase/functions/_shared/database.types.ts
+++ supabase/functions/_shared/database.types.ts
@@ -4,172 +4,176 @@ export type Json =
   | boolean
   | null
   | { [key: string]: Json | undefined }
-  | Json[]
+  | Json[];
 
 export type Database = {
   public: {
     Tables: {
       cards: {
         Row: {
-          choices: Json | null
-          content: Json | null
-          created_at: string
-          id: number
-          leading_choice: string | null
-          parent: number | null
-        }
+          choices: Json | null;
+          content: Json | null;
+          created_at: string;
+          id: number;
+          leading_choice: string | null;
+          parent: number | null;
+        };
         Insert: {
-          choices?: Json | null
-          content?: Json | null
-          created_at?: string
-          id?: number
-          leading_choice?: string | null
-          parent?: number | null
-        }
+          choices?: Json | null;
+          content?: Json | null;
+          created_at?: string;
+          id?: number;
+          leading_choice?: string | null;
+          parent?: number | null;
+        };
         Update: {
-          choices?: Json | null
-          content?: Json | null
-          created_at?: string
-          id?: number
-          leading_choice?: string | null
-          parent?: number | null
-        }
+          choices?: Json | null;
+          content?: Json | null;
+          created_at?: string;
+          id?: number;
+          leading_choice?: string | null;
+          parent?: number | null;
+        };
         Relationships: [
           {
-            foreignKeyName: "cards_parent_fkey"
-            columns: ["parent"]
-            isOneToOne: false
-            referencedRelation: "cards"
-            referencedColumns: ["id"]
+            foreignKeyName: "cards_parent_fkey";
+            columns: ["parent"];
+            isOneToOne: false;
+            referencedRelation: "cards";
+            referencedColumns: ["id"];
           },
-        ]
-      }
+        ];
+      };
       explored_cards: {
         Row: {
-          card_id: number
-          created_at: string
-          game_id: number
-          id: number
-          user_id: string
-        }
+          card_id: number;
+          created_at: string;
+          game_id: number;
+          id: number;
+          user_id: string;
+        };
         Insert: {
-          card_id: number
-          created_at?: string
-          game_id: number
-          id?: number
-          user_id?: string
-        }
+          card_id: number;
+          created_at?: string;
+          game_id: number;
+          id?: number;
+          user_id?: string;
+        };
         Update: {
-          card_id?: number
-          created_at?: string
-          game_id?: number
-          id?: number
-          user_id?: string
-        }
+          card_id?: number;
+          created_at?: string;
+          game_id?: number;
+          id?: number;
+          user_id?: string;
+        };
         Relationships: [
           {
-            foreignKeyName: "explored_cards_card_id_fkey"
-            columns: ["card_id"]
-            isOneToOne: false
-            referencedRelation: "cards"
-            referencedColumns: ["id"]
+            foreignKeyName: "explored_cards_card_id_fkey";
+            columns: ["card_id"];
+            isOneToOne: false;
+            referencedRelation: "cards";
+            referencedColumns: ["id"];
           },
           {
-            foreignKeyName: "explored_cards_game_id_fkey"
-            columns: ["game_id"]
-            isOneToOne: false
-            referencedRelation: "games"
-            referencedColumns: ["id"]
+            foreignKeyName: "explored_cards_game_id_fkey";
+            columns: ["game_id"];
+            isOneToOne: false;
+            referencedRelation: "games";
+            referencedColumns: ["id"];
           },
-        ]
-      }
+        ];
+      };
       games: {
         Row: {
-          created_at: string
-          id: number
-          name: string | null
-        }
+          created_at: string;
+          id: number;
+          name: string | null;
+        };
         Insert: {
-          created_at?: string
-          id?: number
-          name?: string | null
-        }
+          created_at?: string;
+          id?: number;
+          name?: string | null;
+        };
         Update: {
-          created_at?: string
-          id?: number
-          name?: string | null
-        }
-        Relationships: []
-      }
+          created_at?: string;
+          id?: number;
+          name?: string | null;
+        };
+        Relationships: [];
+      };
       users: {
         Row: {
-          created_at: string
-          id: number
-        }
+          created_at: string;
+          id: number;
+        };
         Insert: {
-          created_at?: string
-          id?: number
-        }
+          created_at?: string;
+          id?: number;
+        };
         Update: {
-          created_at?: string
-          id?: number
-        }
-        Relationships: []
-      }
-    }
+          created_at?: string;
+          id?: number;
+        };
+        Relationships: [];
+      };
+    };
     Views: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Functions: {
       get_linked_rows: {
         Args: {
-          start_id: number
-        }
+          start_id: number;
+        };
         Returns: {
-          choices: Json | null
-          content: Json | null
-          created_at: string
-          id: number
-          leading_choice: string | null
-          parent: number | null
-        }[]
-      }
-    }
+          choices: Json | null;
+          content: Json | null;
+          created_at: string;
+          id: number;
+          leading_choice: string | null;
+          parent: number | null;
+        }[];
+      };
+    };
     Enums: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     CompositeTypes: {
-      [_ in never]: never
-    }
-  }
-}
+      [_ in never]: never;
+    };
+  };
+};
 
-type PublicSchema = Database[Extract<keyof Database, "public">]
+type PublicSchema = Database[Extract<keyof Database, "public">];
 
 export type Tables<
   PublicTableNameOrOptions extends
     | keyof (PublicSchema["Tables"] & PublicSchema["Views"])
     | { schema: keyof Database },
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
-    ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
-        Database[PublicTableNameOrOptions["schema"]]["Views"])
+    ? keyof (
+      & Database[PublicTableNameOrOptions["schema"]]["Tables"]
+      & Database[PublicTableNameOrOptions["schema"]]["Views"]
+    )
     : never = never,
-> = PublicTableNameOrOptions extends { schema: keyof Database }
-  ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
-      Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
-      Row: infer R
-    }
-    ? R
+> = PublicTableNameOrOptions extends { schema: keyof Database } ? (
+    & Database[PublicTableNameOrOptions["schema"]]["Tables"]
+    & Database[PublicTableNameOrOptions["schema"]]["Views"]
+  )[TableName] extends {
+    Row: infer R;
+  } ? R
+  : never
+  : PublicTableNameOrOptions extends keyof (
+    & PublicSchema["Tables"]
+    & PublicSchema["Views"]
+  ) ? (
+      & PublicSchema["Tables"]
+      & PublicSchema["Views"]
+    )[PublicTableNameOrOptions] extends {
+      Row: infer R;
+    } ? R
     : never
-  : PublicTableNameOrOptions extends keyof (PublicSchema["Tables"] &
-        PublicSchema["Views"])
-    ? (PublicSchema["Tables"] &
-        PublicSchema["Views"])[PublicTableNameOrOptions] extends {
-        Row: infer R
-      }
-      ? R
-      : never
-    : never
+  : never;
 
 export type TablesInsert<
   PublicTableNameOrOptions extends
@@ -180,17 +184,15 @@ export type TablesInsert<
     : never = never,
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Insert: infer I
-    }
-    ? I
-    : never
+    Insert: infer I;
+  } ? I
+  : never
   : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
     ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
-        Insert: infer I
-      }
-      ? I
-      : never
+      Insert: infer I;
+    } ? I
     : never
+  : never;
 
 export type TablesUpdate<
   PublicTableNameOrOptions extends
@@ -201,17 +203,15 @@ export type TablesUpdate<
     : never = never,
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Update: infer U
-    }
-    ? U
-    : never
+    Update: infer U;
+  } ? U
+  : never
   : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
     ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
-        Update: infer U
-      }
-      ? U
-      : never
+      Update: infer U;
+    } ? U
     : never
+  : never;
 
 export type Enums<
   PublicEnumNameOrOptions extends
@@ -224,19 +224,24 @@ export type Enums<
   ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
   : PublicEnumNameOrOptions extends keyof PublicSchema["Enums"]
     ? PublicSchema["Enums"][PublicEnumNameOrOptions]
-    : never
+  : never;
 
 export type CompositeTypes<
   PublicCompositeTypeNameOrOptions extends
     | keyof PublicSchema["CompositeTypes"]
     | { schema: keyof Database },
   CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
-    schema: keyof Database
+    schema: keyof Database;
   }
-    ? keyof Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
+    ? keyof Database[PublicCompositeTypeNameOrOptions["schema"]][
+      "CompositeTypes"
+    ]
     : never = never,
 > = PublicCompositeTypeNameOrOptions extends { schema: keyof Database }
-  ? Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
-  : PublicCompositeTypeNameOrOptions extends keyof PublicSchema["CompositeTypes"]
+  ? Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][
+    CompositeTypeName
+  ]
+  : PublicCompositeTypeNameOrOptions extends
+    keyof PublicSchema["CompositeTypes"]
     ? PublicSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
-    : never
+  : never;
