ccf7a0458ab19c159920606eed02e0ff98845c82
Ell1ott <85990359+Ell1ott@users.noreply.github.com>
2024-12-13 15:37:00 +1300

    Reduce random rotation

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -146,7 +146,7 @@ export default function GameInterface() {
 		console.log("offset: ", info.offset.x);
 		console.log("velocity: ", info.velocity.x);
 		const predictedX = info.offset.x + info.velocity.x;
-		const predictedY = (info.offset.y + info.velocity.y);
+		const predictedY = info.offset.y + info.velocity.y;
 
 		const offset = Math.sqrt(predictedX ** 2 + (predictedY / 10) ** 2);
 
@@ -164,7 +164,7 @@ export default function GameInterface() {
 
 			setScenario([...scenarios, scenarios.length]);
 			while (scenarios.length + 1 > randomRotations.current.length) {
-				randomRotations.current.push(Math.random() * 10 - 5);
+				randomRotations.current.push((Math.random() - 0.5) * 5);
 			}
 
 			await mainControls.start({
@@ -240,9 +240,9 @@ export default function GameInterface() {
 	};
 
 	const randomRotations = useRef([
-		Math.random() * 10 - 5,
-		Math.random() * 10 - 5,
-		Math.random() * 10 - 5,
+		(Math.random() - 0.5) * 5,
+		(Math.random() - 0.5) * 5,
+		(Math.random() - 0.5) * 5,
 	]);
 
 	// useEffect(() => {
@@ -278,74 +278,84 @@ export default function GameInterface() {
 				<div className="relative w-full aspect-[6/7]">
 					<AnimatePresence>
 						{scenarios[1] &&
-							scenarios.map((i) => (
-								<motion.div
-									key={`${i}`}
-									animate={{
-										scale:
-											0.95 ** (i - (currentScenarioIndex + (isAnimating && 1))),
-										y: (i - (currentScenarioIndex + (isAnimating && 1))) * 30,
-										opacity: 1,
-									}}
-									initial={{
-										y: (i - currentScenarioIndex) * 30,
-										opacity: 0,
-										scale: 0.95 ** (i - currentScenarioIndex),
-									}}
-									style={{
-										zIndex: scenarios.length - i,
-										visibility:
-											i >= currentScenarioIndex ? "visible" : "hidden",
-										rotate: randomRotations.current[i],
-										willChange: "transform, opacity", // Hint the browser
-									}}
-									whileTap={{ cursor: "grabbing" }}
-									className="absolute inset-0 touch-none"
-									transition={{ duration: 0.5, ease: "easeInOut" }}
-								>
-									<motion.div
-										className="absolute inset-0 bg-black rounded-2xl shadow-xl"
-										id={i + ""}
-										style={i === currentScenarioIndex ? { rotate, x, y } : {}}
-										drag={i === currentScenarioIndex && !isAnimating}
-										animate={i === currentScenarioIndex && mainControls}
-										dragConstraints={{
-											top: 0,
-											bottom: 0,
-											left: -100,
-											right: 100,
-										}}
-										onDragEnd={
-											i === currentScenarioIndex ? handleDragEnd : undefined
-										}
-									>
+							scenarios.map(
+								(i) =>
+									i >= currentScenarioIndex && (
 										<motion.div
-											className="p-6 h-full flex flex-col bg-neutral-800 rounded-2xl"
+											key={`${i}`}
 											animate={{
-												opacity:
-													1 -
+												scale:
+													0.95 **
+													(i - (currentScenarioIndex + (isAnimating && 1))),
+												y:
 													(i - (currentScenarioIndex + (isAnimating && 1))) *
-														0.2,
+													30,
+												opacity: 1,
+												
 											}}
 											initial={{
-												opacity:
-													1 -
-													(i - (currentScenarioIndex + (isAnimating && 1))) *
-														0.4,
+												y: (i - currentScenarioIndex) * 30,
+												opacity: 0,
+												scale: 0.95 ** (i - currentScenarioIndex),
 											}}
+											style={{
+												zIndex: scenarios.length - i,
+												rotate: randomRotations.current[i],
+
+												willChange: "transform, opacity", // Hint the browser
+											}}
+											whileTap={{ cursor: "grabbing" }}
+											className="absolute inset-0 touch-none"
+											transition={{ duration: 0.5, ease: "easeInOut" }}
 										>
-											<div className="flex-1 flex flex-col text-center items-center justify-around">
-												<p className="font-mono text-sm md:text-base">
-													{i === currentScenarioIndex
-														? currentScenario.situation
-														: nextCardContent}
-												</p>
-												<div className="w-32 h-32 bg-neutral-700 rounded-full" />
-											</div>
+											<motion.div
+												className="absolute inset-0 bg-black rounded-2xl shadow-xl"
+												id={i + ""}
+												style={
+													i === currentScenarioIndex ? { rotate, x, y } : {}
+												}
+												drag={i === currentScenarioIndex && !isAnimating}
+												animate={i === currentScenarioIndex && mainControls}
+												dragConstraints={{
+													top: 0,
+													bottom: 0,
+													left: -100,
+													right: 100,
+												}}
+												onDragEnd={
+													i === currentScenarioIndex ? handleDragEnd : undefined
+												}
+											>
+												<motion.div
+													className="p-6 h-full flex flex-col bg-neutral-800 rounded-2xl"
+													animate={{
+														opacity:
+															1 -
+															(i -
+																(currentScenarioIndex + (isAnimating && 1))) *
+																0.2,
+													}}
+													initial={{
+														opacity:
+															1 -
+															(i -
+																(currentScenarioIndex + (isAnimating && 1))) *
+																0.4,
+													}}
+												>
+													<div className="flex-1 flex flex-col text-center items-center justify-around">
+														<p className="font-mono text-sm md:text-base">
+															{i === currentScenarioIndex
+																? currentScenario.situation
+																: nextCardContent}
+														</p>
+														<div className="w-32 h-32 bg-neutral-700 rounded-full" />
+													</div>
+												</motion.div>
+											</motion.div>
 										</motion.div>
-									</motion.div>
-								</motion.div>
-							))}
+									)
+							)}
 					</AnimatePresence>
 				</div>
 				<div className="mt-8 flex flex-row gap-6 font-mono px-0 w-full justify-between md:text-xl z-10">
