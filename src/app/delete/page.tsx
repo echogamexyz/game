916f8ca6ac54cda4144519e23ff2224b4411ffef
Jonathan Bangert <jonathan@bangert.dk>
2024-12-12 19:24:40 -0700

    Add delete page

---
+++ src/app/delete/page.tsx
@@ -0,0 +1,13 @@
+// Page with information to delete user data
+
+export default function DeletePage() {
+	return (
+		<div>
+			<h1>Delete your data</h1>
+			<p>
+				If you want to delete your data, please send an email to{" "}
+				<a href="mailto:delete@novusgroup.dk" />
+			</p>
+		</div>
+	);
+}
