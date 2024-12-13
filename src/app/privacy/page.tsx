0262b222bcdb2f1e90393f1b8eb088489efb182f
Jonathan Bangert <jonathan@bangert.dk>
2024-12-12 23:42:45 -0700

    Add privacy page

---
+++ src/app/privacy/page.tsx
@@ -0,0 +1,17 @@
+// Privacy policy page
+
+export default function PrivacyPage() {
+	return (
+		<div>
+			<h1>Privacy Policy</h1>
+			<p>
+				This privacy policy applies to the use of the website novusgroup.dk.
+			</p>
+			<p>We do not collect any personal data from our users.</p>
+			<p>
+				If you have any questions regarding our privacy policy, please contact
+				us at <a href="mailto:contact@novusgroup.dk" />
+			</p>
+		</div>
+	);
+}
